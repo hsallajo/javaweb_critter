bd1a152e01605ea3455c28aea201a480376e0181
shuopone <hsallajo@gmail.com>
2021-11-28 15:23:48 -0800

    init

---
+++ src/main/java/com/shu/jdnd/course3/critter/user/UserController.java
@@ -0,0 +1,52 @@
+package com.shu.jdnd.course3.critter.user;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.time.DayOfWeek;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * Handles web requests related to Users.
+ *
+ */
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @PostMapping("/customer")
+    public CustomerDTO saveCustomer(@RequestBody CustomerDTO customerDTO){
+        throw new UnsupportedOperationException();
+    }
+
+    @GetMapping("/customer")
+    public List<CustomerDTO> getAllCustomers(){
+        throw new UnsupportedOperationException();
+    }
+
+    @GetMapping("/customer/pet/{petId}")
+    public CustomerDTO getOwnerByPet(@PathVariable long petId){
+        throw new UnsupportedOperationException();
+    }
+
+    @PostMapping("/employee")
+    public EmployeeDTO saveEmployee(@RequestBody EmployeeDTO employeeDTO) {
+        throw new UnsupportedOperationException();
+    }
+
+    @PostMapping("/employee/{employeeId}")
+    public EmployeeDTO getEmployee(@PathVariable long employeeId) {
+        throw new UnsupportedOperationException();
+    }
+
+    @PutMapping("/employee/{employeeId}")
+    public void setAvailability(@RequestBody Set<DayOfWeek> daysAvailable, @PathVariable long employeeId) {
+        throw new UnsupportedOperationException();
+    }
+
+    @GetMapping("/employee/availability")
+    public List<EmployeeDTO> findEmployeesForService(@RequestBody EmployeeRequestDTO employeeDTO) {
+        throw new UnsupportedOperationException();
+    }
+
+}
---
+++ src/test/java/com/shu/jdnd/course3/critter/CritterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shu.jdnd.course3.critter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CritterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
